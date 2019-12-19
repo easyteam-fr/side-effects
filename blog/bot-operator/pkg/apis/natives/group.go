9a92d9e9730f332dd30f70cbf3a8aadb76d34dfb
gregoryguillou <gregory.guillou@resetlogs.com>
2019-12-19 10:58:47 +0100

    [blog] bot-operator

---
+++ blog/bot-operator/pkg/apis/natives/group.go
@@ -0,0 +1,6 @@
+// Package natives contains natives API versions.
+//
+// This file ensures Go source parsers acknowledge the natives package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package natives
---
+++ blog/bot-operator/pkg/apis/natives/v1alpha1/bot_types.go
@@ -0,0 +1,46 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// BotSpec defines the desired state of Bot
+type BotSpec struct {
+	// +kubebuilder:validation:MaxLength=15
+	// +kubebuilder:validation:MinLength=1
+	Alias string `json:"alias,omitempty"`
+}
+
+// BotStatus defines the observed state of Bot
+type BotStatus struct {
+	lastState string `json:"last_state,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Bot is the Schema for the bots API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=bots,scope=Namespaced
+type Bot struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BotSpec   `json:"spec,omitempty"`
+	Status BotStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// BotList contains a list of Bot
+type BotList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Bot `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Bot{}, &BotList{})
+}
