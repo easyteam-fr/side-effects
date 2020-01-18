c43b51e0cef7da91d481b3a3ad0c9c0c7ca9c4de
Easyteam <58460838+easyteam-fr@users.noreply.github.com>
2020-01-18 21:04:23 +0100

    Simple Prometheus application (#8)

---
+++ blog/prometheus-app/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type message struct {
+	Text *string `json:"text,omitempty"`
+}
+
+var (
+	invocations = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "app_requests_by_uri",
+		Help: "Number of calls by URL",
+	},
+		[]string{"url"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(invocations)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	invocations.WithLabelValues(r.URL.Path).Inc()
+	text := "Helloworld!"
+	msg := &message{
+		Text: &text,
+	}
+	output, err := json.Marshal(msg)
+	if err != nil {
+		log.Panicf("error %v", err)
+	}
+	fmt.Fprintln(w, string(output))
+}
+
+func main() {
+	finish := make(chan bool)
+
+	server1 := http.NewServeMux()
+	server2 := http.NewServeMux()
+
+	server2.Handle("/metrics", promhttp.Handler())
+	server1.HandleFunc("/url1", handler)
+	server1.HandleFunc("/url2", handler)
+
+	go func() { log.Fatal(http.ListenAndServe(":8080", server1)) }()
+	go func() { log.Fatal(http.ListenAndServe(":8081", server2)) }()
+
+	<-finish
+}
