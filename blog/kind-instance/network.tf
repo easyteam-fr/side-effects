00889b6cd7b82eafa600e0ad6ad0a0dddfb9a3b8
gregoryguillou <gregory.guillou@resetlogs.com>
2020-02-12 08:55:55 +0100

    [kind] add a simple instance

--- blog/kind-instance/network.tf
+++ blog/kind-instance/network.tf
@@ -1,59 +1,22 @@
-resource "aws_vpc" "default" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "random_id" "key" {
   byte_length = 8
 }
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_route53_zone" "private" {
-  name = "${random_id.key.hex}.local"
-  vpc {
-    vpc_id = aws_vpc.default.id
-  }
-}
-
 data "aws_availability_zones" "az" {}
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.default.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.default.id
-}
-
-resource "aws_subnet" "public" {
-  count                   = 1
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  availability_zone       = data.aws_availability_zones.az.names[count.index]
-  map_public_ip_on_launch = true
-}
-
-resource "aws_route_table" "public" {
-  count  = 1
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default.id
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = 1
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public[count.index].id
+data "aws_subnet" "public" {
+  vpc_id                  = data.aws_vpc.default.id
+  availability_zone       = data.aws_availability_zones.az.names[0]
 }
 
 resource "aws_security_group" "default" {
   name        = "${random_id.key.hex}-default-sg"
   description = "Used in the terraform"
-  vpc_id      = aws_vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
